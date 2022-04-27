b49a6058c58496183a37ddb03159536c4d636ba2
BramHamaekers <bram.hamaekers@gmail.com>
2022-04-27 15:29:46 +0200

    CMGenerator Documentation

--- src/swop/UI/Generators/CarMechanicGenerator.java
+++ src/swop/UI/Generators/CarMechanicGenerator.java
@@ -10,7 +10,11 @@ import java.util.Set;
 
 public class CarMechanicGenerator extends UserGenerator {
 
-    //TODO: More descriptive name? + documentation
+    /**
+     * Given a list of workstations display this list
+     * @param builder the builder used in displaying the workStations
+     * @param workStations the workstations to display
+     */
     public void generateStationList(FormBuilder builder, List<WorkStation> workStations) {
         builder.appendTitle("Current Stations");
         ListIterator<WorkStation> w = workStations.listIterator();
@@ -21,6 +25,13 @@ public class CarMechanicGenerator extends UserGenerator {
       //builder.addOption("Select station", workStations.size()); // breaks the use case tests
     }
 
+    /**
+     * Given a list of tasks of a workstation, display the status of these tasks
+     * @param builder the builder used in displaying the status of tasks
+     * @param name name of the workstation
+     * @param pendingTasks list of pending tasks on a workstation
+     * @param finishedTasks list of finished tasks on a workstation
+     */
     public void generateWorkStationStatus(FormBuilder builder, String name, List<Task> pendingTasks, List<Task> finishedTasks) {
         builder.appendTitle(name);
         builder.appendSubTitle("Pending");
@@ -29,6 +40,11 @@ public class CarMechanicGenerator extends UserGenerator {
         if (finishedTasks != null) finishedTasks.forEach(t -> builder.inputInfo(t.getName()));
     }
 
+    /**
+     * Given a list of availableTasks, display these tasks
+     * @param builder the builder used in displaying the tasks
+     * @param taskList list of available Tasks to display
+     */
     public void generateAvailableTasks(FormBuilder builder, List<Task> taskList){
         builder.appendTitle("Available tasks");
         ListIterator<Task> it = taskList.listIterator();
@@ -39,6 +55,11 @@ public class CarMechanicGenerator extends UserGenerator {
         builder.endInfo();
     }
 
+    /**
+     * Given a list of info, display this info
+     * @param builder the builder used in displaying the info
+     * @param info the info to display
+     */
     public void generateTaskInfo(FormBuilder builder, List<String> info){
         builder.appendSubTitle("Info For Task");
         for (String s : info) {
